a3f78f86e77f591f44054861d9945f2c7d5c0f48
Sagar Chaudhary <sagar.c@productivet.com>
2019-03-07 11:32:17 +0530

    project modal

--- src/app/project-modal/project-modal.component.ts
+++ src/app/project-modal/project-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
-
+import { DataService } from './../../services/data.service'; 
 @Component({
   selector: 'app-project-modal',
   templateUrl: './project-modal.component.html',
@@ -9,12 +9,22 @@ export class ProjectModalComponent implements OnInit {
   @Input()  isEdited;
   @Input() item;
   @Output() addProject = new EventEmitter<object>();
-  constructor() { }
+  modules = [];
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getAll('module')
+    .subscribe(data => {
+        this.modules = data.data;
+    });
   }
 
   add(item) {
    this.addProject.emit(item);
+  }
+
+  closeModelOnCross() {
+    document.getElementById('id01').style.display = 'none';
+    document.body.style.overflow = 'auto';
   }
 }
